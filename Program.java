e70ef3e1551dc53dc210c61f82b1c49f674a431b
vivimila <violamilashevskaya@yandex.by>
2023-01-29 18:58:00 +0300

    Add 'reader close'

--- Program.java
+++ Program.java
@@ -8,6 +8,7 @@
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+// import java.util.Comparator;
 import java.util.LinkedList;
 
 public class Program
@@ -27,26 +28,46 @@ public class Program
         while (reader. ready ()) {
             text += (char) reader. read();
         }
-        
+        reader.close();
         String [] str = text.split("\r\n");
         for (int i = 1; i < str.length; i++){
             String [] sb = str[i].split(" ");
 
-            family.add(sb[0]);
-            name.add(sb[1]);
-            soname.add(sb[2]);
+            family.add(sb[0] + " ");
+            name.add(sb[1].substring(0, 1) + ".");
+            soname.add(sb[2].substring(0, 1) + "." + " ");
             age.add(Integer.valueOf(sb[3]));
             gender.add(sb[4] == "M" ? true : false);
             index.add(i);
-
-             System.out.println(family); 
-
-
-            // System.out.println(sb[0] + " " + sb[1].charAt(0) + "." + sb[2].charAt(0) + "." + " " + sb[3] + " " + sb[4]);
         }
         
+        // new *
+        //index. sort(new Comparator<Integer>() {
+        //   new *
+        //   @Override
+        //   public int compare(Integer o1, Integer o2)  {
+        //       return o2-o1;
+        //   }        
+        //});
 
+        for (int i = 0; i < index.size(); i++){
+            System.out.printf(family.get(i));
+            System.out.printf(name.get(i));
+            System.out.printf(soname.get(i));
+            System.out.printf(age.get(i).toString());
+            System.out.printf((gender.get(i) ? "M" : "Ж"));
+            System.out.println();
+        }
+
+            // System.out.println(family); 
+
+            // System.out.println(sb[0] + " " + sb[1].charAt(0) + "." + sb[2].charAt(0) + "." + " " + sb[3] + " " + sb[4]);
+        
     }
+    
 }
+
+    
+
 
    
